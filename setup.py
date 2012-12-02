bc59dd32c876aa34f78c8bcb48dc51e0f42818af
Alex Hammel <ahammel87@gmail.com>
2012-12-02 09:27:49 -0800

    First commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='OperatorStr',
+    version='0.1.0',
+    author='Alex Hammel',
+    author_email='ahammel87@gmail.com',
+    packages=[],
+    scripts=[],
+    url='http://pypi.python.org/pypi/OperatorStr/',
+    license='LICENSE.txt',
+    description='',
+    long_description=open('README.txt').read(),
+    install_requires=[ ],
+    )
